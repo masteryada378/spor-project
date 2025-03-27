2bf5b4fff50ee57df76800c1e0c834dab2ede83b
Yurii <yurii@Yuriis-MacBook-Pro.local>
2025-03-27 13:31:05 +0200

    clining

--- backend/src/routes/cards.ts
+++ backend/src/routes/cards.ts
@@ -1,15 +1,3 @@
-// import express from 'express';
-// import { getAllCards, createCard, updateCard, deleteCard } from '../controllers/cards.js';
-
-// const router = express.Router();
-
-// router.get('/', getAllCards);
-// router.post('/', createCard);
-// router.put('/:id', updateCard);
-// router.delete('/:id', deleteCard);
-
-// export { router as cardRoutes };
-
 import express from 'express';
 import { getCards, createCard, updateCard, deleteCard } from '../controllers/cards';
 

b8321a6b554f5dbf25f305041f60b462774ec74d
Yurii <yurii@Yuriis-MacBook-Pro.local>
2025-04-15 13:31:18 +0300

    fix lint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import cors from 'cors';
 import { json } from 'body-parser';
 import cardRoutes from './routes/cards';
